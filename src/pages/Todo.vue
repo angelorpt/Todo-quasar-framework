f00a5f035f10ca9a9d45428a6b7d64ec0df1cd87
Angelo <angelorpt@gmail.com>
2020-02-20 11:00:17 -0400

    Adicionada a lista de tarefas

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,11 +1,57 @@
 <template>
-  <q-page>
-    <h1>Todo</h1>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white" separator bordered>
+
+      <q-item
+        v-ripple
+        v-for="(task) in tasks"
+        :key="task.index"
+        @click="task.done = !task.done"
+        :class="{ 'done bg-blue-1' : task.done }"
+        clickable >
+
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" color="primary" class="no-pointer-events"/>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      tasks: [
+        {
+          title: 'Get bananas',
+          done: false
+        },
+        {
+          title: 'Eat bananas',
+          done: false
+        },
+        {
+          title: 'Poo bananas',
+          done: false
+        }
+      ]
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+  .done {
+    .q-item__label {
+      text-decoration: line-through;
+      color: #bbb;
+    }
+  }
+</style>
