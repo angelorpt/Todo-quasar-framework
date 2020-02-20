e475022c6f3a6fb238de6fdeac96090880afac5f
Angelo <angelorpt@gmail.com>
2020-02-20 10:40:33 -0400

    Página de help

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,11 +1,13 @@
 <template>
-  <q-page>
-    <h1>Help</h1>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet nisi et orci suscipit porttitor. Etiam tempor, tellus et venenatis vulputate, velit purus blandit odio, laoreet gravida nulla mi sit amet justo. Sed ullamcorper lacinia luctus. Mauris non consectetur diam. Suspendisse condimentum suscipit quam, id lobortis mi molestie id. Donec neque metus, malesuada semper risus eget, facilisis rhoncus dui. Pellentesque molestie efficitur sollicitudin. Duis quam justo, ultrices at nunc sed, iaculis vulputate dolor. Aliquam pretium enim leo, sit amet pretium magna vehicula ut. In sagittis vehicula bibendum.</p>
+    <p>Donec sed ultricies justo. Vivamus nunc nibh, posuere ut purus in, euismod tincidunt tellus. Integer non sodales lorem. Nullam turpis nisi, tincidunt eget porta nec, tincidunt non urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras non justo euismod, dignissim mauris at, vulputate justo. Quisque vel nibh dolor. Vivamus congue aliquet volutpat. Praesent ac diam massa. In nibh mi, sollicitudin non erat vitae, fringilla ultricies mauris. </p>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'Help'
+
 }
 </script>
